4506f48adfb663ab923b7ae300cc10ab3069e590
maralmeida <maralmeida22@gmail.com>
2025-11-27 22:20:38 -0500

    genera ID y trazabilidad en 2 componentes, stream de log en archivo log y consola

---
+++ TrazabilidadyLogs/IngresoProducto.js
@@ -0,0 +1,5 @@
+const {logger, traceID} = require('./utils');
+
+
+// mensaje de prueba para verificar que el logger y la generación de ID funcionan, para validar quitar comentario si tiene // al inicio
+logger.info("Logger inicializado correctamente, genera ID funcionando, ejemplo "+ traceID+" Componente IngresoProducto.js"  );
---
+++ TrazabilidadyLogs/utils.js
@@ -0,0 +1,32 @@
+//--> crea ID trazas, logger consola y .log convirtiendo level y fecha en formato legible
+
+
+const { multistream, destination} = require('pino');
+// genero id's unicos con uuid v4 y asigno id a variable
+const {v4:uuidv4} = require ('uuid');
+const traceIDgenerator = uuidv4();
+
+// genero logger
+const logger = require ('pino')({
+    level: 'trace',
+    formatters:{
+        level(label) { return{level: label}; } // Cambia número nivel de log por texto
+    },
+    timestamp: () => ` ,"time": "${ new Date().toISOString() }" `  // convierte timestamp  de solo numeros a ISO 8601
+    },
+    multistream([
+        {stream: process.stdout},           
+        {stream: destination( { dest: 'logsapi.log', sync:false}) } 
+    ])
+);
+
+// exporto ID unico para trazabilidad 
+exports.traceID = traceIDgenerator;
+// exporto logger con nombre logger para uso global
+exports.logger = logger;
+
+
+// mensaje de prueba para verificar que el logger y la generación de ID funcionan, para validar quitar comentario si tiene // al inicio
+logger.info("Logger inicializado correctamente, genera ID funcionando, ejemplo "+ traceIDgenerator+" Componente utils.js"  );
+
+
